116d4b441fcc7da0b118962154d3ce532ac57a41
Tran Duc Loc <109507062+trducloc@users.noreply.github.com>
2023-12-01 22:12:07 +0700

    Update main.js

--- Buoi3/main.js
+++ Buoi3/main.js
@@ -15,22 +15,17 @@ textParagraph.style.fontSize = "2rem";
 textParagraph.innerHTML = 'Tôi có thể làm <em>bất cứ điều gì</em> tôi muốn với JavaScript.';
 
 // Bai 2 : Sử dụng vòng lặp để đặt màu chữ cho tất cả thẻ li thành màu blue (tạo thẻ ul-li bằng html)
-var listItems = document.getElementById("myList").getElementsByTagName("li");
 
+var listItems = document.getElementById("myList").getElementsByTagName("li");
 for (var i = 0; i < listItems.length; i++) {
     listItems[i].style.color = "blue";
 }
 
 /* Bai 3: Sử dụng javascript để thực hiện những công việc sau
-
 Thêm 3 thẻ <li> có nội dung Item 8, Item 9, Item 10 vào cuối danh sách
-
 Sửa nội dung cho thẻ <li> 1 thành màu đỏ (color)
-
 Sửa background cho thẻ <li> 3 thành màu xanh (background-color)
-
 Remove thẻ <li> 4
-
 Thêm thẻ <li> mới thay thế cho thẻ <li> 4 bị xóa ở bước trước, thẻ <li> mới có nội dung bất kỳ */
 
 // Thêm 3 thẻ <li> vào cuối danh sách
